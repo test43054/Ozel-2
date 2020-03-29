25324d23002644867c73f4efb185c98ed3000ab2
Mustafa Yağız ORAL <mustafa.oral@outlook.com.tr>
2020-03-29 04:18:07 +0300

    Video 4 Model Category ve Product ilişkisi
    category ve product arasındaki ilişkiyi kurdum ve product tablosu oluşturup admine ekledim

--- product/admin.py
+++ product/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from product.models import Category
+from product.models import Category, Product
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,4 +10,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ['status']
 
 
-admin.site.register(Category,CategoryAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status']
+    list_filter = ['status']
+
+
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -10,12 +11,31 @@ class Category(models.Model):
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True,upload_to='images/')
-    status=models.CharField(max_length=10, choices=STATUS)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
-    parent = models.ForeignKey('self',blank=True, null=True ,related_name='children', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
-    uptade_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
 
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
