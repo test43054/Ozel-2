b055cf8ad3a44af5ce52a908706a9188c530f183
Mustafa Yağız ORAL <mustafa.oral@outlook.com.tr>
2020-04-01 03:16:28 +0300

    !!Düzeltme!! Video 6 hazır template ekleme
    !!Düzeltme!! Video 6 hazır template ekleme

--- product/admin.py
+++ product/admin.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from product.models import Category, Product, Images
+from product.models import Category, Product
 
-class ProductImageInline(admin.TabularInline):
-    model = Images
-    extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
@@ -16,13 +13,8 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'category', 'status']
     list_filter = ['status']
-    inlines = [ProductImageInline]
 
-class ImagesAdmin(admin.ModelAdmin):
-    list_display = ['title', 'product', 'image']
 
 admin.site.register(Category, CategoryAdmin)
 
 admin.site.register(Product, ProductAdmin)
-
-admin.site.register(Images, ImagesAdmin)
--- product/models.py
+++ product/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -27,7 +27,7 @@ class Product(models.Model):
         ('False', 'Hayır'),
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -37,12 +37,5 @@ class Product(models.Model):
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-    def __str__(self):
-        return self.title
-
-class Images(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50, blank=True)
-    image = models.ImageField(blank=True, upload_to='images/')
     def __str__(self):
         return self.title
