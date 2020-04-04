941223d75e2774d552bbafc2e0ebdc81ae7b49c8
Mustafa Yağız ORAL <mustafa.oral@outlook.com.tr>
2020-04-05 02:54:32 +0300

    Video 13 Django Contact form bize ulaşın formu oluşturuldu
    Video 13 Django Contact form bize ulaşın formu oluşturuldu

--- home/views.py
+++ home/views.py
@@ -1,26 +1,46 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
-  setting = Setting.objects.get(pk=1)
-  context = {'setting': setting, 'page': 'home'}
-  return render(request, 'index.html', context)
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'home'}
+    return render(request, 'index.html', context)
+
 
 def hakkimizda(request):
-  setting = Setting.objects.get(pk=1)
-  context = {'setting': setting, 'page': 'hakkimizda'}
-  return render(request, 'hakkimizda.html', context)
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'hakkimizda'}
+    return render(request, 'hakkimizda.html', context)
+
 
 def referanslar(request):
-  setting = Setting.objects.get(pk=1)
-  context = {'setting': setting, 'page': 'hakkimizda'}
-  return render(request, 'referanslarimiz.html', context)
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page': 'hakkimizda'}
+    return render(request, 'referanslarimiz.html', context)
+
 
 def iletisim(request):
-  setting = Setting.objects.get(pk=1)
-  context = {'setting': setting, 'page': 'hakkimizda'}
-  return render(request, 'iletisim.html', context)
+
+    if request.method == 'POST':  # FORM POST EDİLDİYSE
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()  # model ile bağlantı kur
+            data.name = form.cleaned_data['name']  # formsdan bilgiyi al
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()  # veritabanına kaydet
+            messages.success(request, "mesajınız kaydedilmiştir iyi günler dileriz")
+            return HttpResponseRedirect ('/iletisim')
+
+    setting = Setting.objects.get(pk=1)
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
+    return render(request, 'iletisim.html', context)
+
