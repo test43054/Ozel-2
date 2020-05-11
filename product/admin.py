2999c7c25281c6f1159400a41d19c3cd79c885a8
Mustafa Yağız ORAL <mustafa.oral@outlook.com.tr>
2020-05-12 02:55:58 +0300

    video 22  Kategori ve evler için otomatik slag oluşturma
    video 22  Kategori ve evler için otomatik slag oluşturma

--- product/admin.py
+++ product/admin.py
@@ -12,6 +12,7 @@ class CategoryAdmin2(DraggableMPTTAdmin):
     list_display = ('tree_actions', 'indented_title',
                     'related_products_count', 'related_products_cumulative_count')
     list_display_links = ('indented_title',)
+    prepopulated_fields = {'slug': ('title',)}
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -57,6 +58,7 @@ class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('image_tag',)
     list_filter = ['status']
     inlines = [ProductImageInline]
+    prepopulated_fields = {'slug': ('title',)}
 
 
 class ImagesAdmin(admin.ModelAdmin):
