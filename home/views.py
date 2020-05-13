76bae47335b7e662cb1d8523a232bc5f206b4704
Mustafa Yağız ORAL <mustafa.oral@outlook.com.tr>
2020-05-13 03:37:00 +0300

    video 23 Django Anasayfa da User Login ve logout İşlemleri
    video 23 Django Anasayfa da User Login ve logout İşlemleri

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.contrib import messages
+from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
@@ -120,3 +121,26 @@ def product_search_auto(request):
         data = 'fail'
     mimetype = 'application/json'
     return HttpResponse(data, mimetype)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+
+        else:
+            messages.warning(request,'Giriş başarılı olmadı, kontrol edin!!!!')
+            return HttpResponseRedirect('/login')
+
+    category = Category.objects.all()
+    context = {'category': category,
+
+              }
+    return render(request, 'login.html', context)
